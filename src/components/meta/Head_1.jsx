2d2434df8d3a78c4760f8b67d2e99fe4bf13e875
이서은 <000dl@naver.com>
2024-01-17 10:47:13 +0900

    head 파비콘 추가

--- src/components/meta/Head_1.jsx
+++ src/components/meta/Head_1.jsx
@@ -17,6 +17,13 @@ export default function Head_1() {
           crossOrigin="true"
         />
         {/*<meta name="viewport" content="width=640,user-scalable=yes" />*/}
+        <link rel='icon' href='data:,' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/favicon/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon/favicon-16x16.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='192x192' href='/favicon/favicon-192x192.png' />
+        <link rel='icon' type='image/png' sizes='512x512' href='/favicon/favicon-512x512.png' />
+        <link rel='icon' href='/favicon/favicon.ico' />
       </head>
     </>
   );
